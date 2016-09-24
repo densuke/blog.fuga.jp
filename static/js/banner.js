342bc6ec2038461ab0548633d79ee4741ead26aa
Sato Daisuke <densuke@fuga.jp>
2016-09-24 15:35:43 +0900

    Minergateリンクを修正

--- static/js/banner.js
+++ static/js/banner.js
@@ -9,7 +9,7 @@ var bannerVerticals = [
   <img src="http://hbb.afl.rakuten.co.jp/hsb/15006497.2b81f2fe.14bde460.01c367fd/?me_id=1&me_adv_id=1391319&t=pict" border="0" style="margin:2px"></a>',
   '<a href="https://bitflyer.jp?bf=uvqmgxlj" target="_blank"><img src="https://bitflyer.jp/Images/Affiliate/affi_05_120x600.gif" alt="bitFlyer ビットコインを始めるなら安心・安全な取引所で"></a>',
   "<iframe data-aa='226992' src='https://ad.a-ads.com/226992?size=160x600' scrolling='no' style='width:160px; height:600px; border:0px; padding:0;overflow:hidden' allowtransparency='true' frameborder='0'></iframe>",
-  '<a href="https://minergate.com"><img src="https://minergate.com/assets/promo/120x600-0.png"/></a>'
+  '<a href="https://minergate.com/a/1a1f28ccb912cd314b657388"><img src="https://minergate.com/assets/promo/120x600-0.png"/></a>'
 ];
 
 var bannerMinis = [
@@ -22,7 +22,7 @@ var bannerMinis = [
   '<a href="http://www.pointtown.com/ptu/regist/friendship.do?guid=ON&found=100166&intrid=VSuZLVsTtQS5i&cid=6174" target="_blank">\
   <img src="http://img.pointtown.com/image/page/friendship/banner/ptF_2_120x60.png" border="0" alt="ポイントでお小遣い稼ぎ｜ポイントタウン"></a>',
   '<a href="https://bitflyer.jp?bf=uvqmgxlj" target="_blank"><img src="https://bitflyer.jp/Images/Affiliate/affi_05_120x120.gif" alt="bitFlyer ビットコインを始めるなら安心・安全な取引所で"></a>',
-  '<a href="https://minergate.com"><img src="https://minergate.com/assets/promo/100x100-0.png"/></a>',
+  '<a href="https://minergate.com/a/1a1f28ccb912cd314b657388"><img src="https://minergate.com/assets/promo/100x100-0.png"/></a>',
   '<a href="http://fx-on.com/re/?i=X%2FVGZcqF5w%2FRknpEylMJqDSseqVDB4sDBypHp8piExY" target="_blank" style="font-size: 12px; font-weight: bold;">優秀トレーダーの<br />投資法をゲット！<br /><img src="http://fx-on.com/asp/img/banner/port01_160.jpg" alt="FX投資情報コミュニティfx-on.com" style="border: 0;" /></a><img border="0" width="1" height="1" src="http://fx-on.com/0.php?p=1&a=2742">'
 ];
 
@@ -34,7 +34,7 @@ var bannerHorizontals = [
   '<a href="http://click.linksynergy.com/fs-bin/click?id=npSKtIuxFQc&offerid=47523.10011521&type=4&subid=0"><img alt="ベルメゾンネット" border="0" src="http://www2.bellemaison.jp/pc/premoni/ad/lsimg/bn_trend05_468_60.gif" /></a><img border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=npSKtIuxFQc&amp;bids=47523.10011521&amp;type=4&amp;subid=0" />',
   "<iframe data-aa='224167' src='https://ad.a-ads.com/224167?size=468x60' scrolling='no' style='width:468px; height:60px; border:0px; padding:0;overflow:hidden' allowtransparency='true' frameborder='0'></iframe>",
   '<script language="javascript" src="//ad.jp.ap.valuecommerce.com/servlet/smartphonebanner?sid=3301980&pid=884273138&position=inline"></script>',
-  '<a href="https://minergate.com"><img src="https://minergate.com/assets/promo/728x90-0.png"/></a>',
+  '<a href="https://minergate.com/a/1a1f28ccb912cd314b657388"><img src="https://minergate.com/assets/promo/728x90-0.png"/></a>',
   '<iframe src="http://blog.fx-on.com/parts/product_choice_parts.php?na=%E4%BD%BF%E3%81%A3%E3%81%A6%E3%82%8B%E8%87%AA%E5%8B%95%E5%8F%96%E5%BC%95%26%E8%A3%81%E9%87%8F%E5%8F%96%E5%BC%95%E3%83%84%E3%83%BC%E3%83%AB%E9%81%94%E3%81%A7%E3%83%BC%E3%81%99&s=0&w=600&h=180&fr=47A1DE&fo=FFFFFF&m=3&u=2742&pa=146" style="width:610px;height:200px;" frameborder="0" scrolling="no"></iframe>'
 ];
 
